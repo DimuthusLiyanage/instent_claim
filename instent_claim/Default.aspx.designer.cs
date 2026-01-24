487c6e55911f6a50f0d17da2360743cec02c3618
DimuthusLiyanage <57349291+DimuthusLiyanage@users.noreply.github.com>
2026-01-24 23:22:01 +0530

    Create some actractive login page and  home page

--- instent_claim/Default.aspx.designer.cs
+++ instent_claim/Default.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace instent_claim
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// lblUserInitial control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUserInitial;
+
+        /// <summary>
         /// lblUserName control.
         /// </summary>
         /// <remarks>
@@ -49,5 +58,14 @@ namespace instent_claim
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogout;
+
+        /// <summary>
+        /// lblWelcomeName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblWelcomeName;
     }
 }
